0443124a8108857db3abc54ec97e704e83228a10
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2015-06-03 10:42:58 +0100

    Adding winmove key bindings and clj-refactor

--- personal/init.el
+++ personal/init.el
@@ -1,1 +1,20 @@
-(prelude-require-package 'monokai-theme)
+;;; init.el --- personal configuration entry point.
+;;
+
+;;; Commentary:
+;;
+
+;;; Code:
+(prelude-require-packages '(monokai-theme clj-refactor))
+
+;; move between windows with Shift+arrows
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+
+;; clj-refactor
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")))
+
+;;; init.el ends here
